9cb383b55c717deaa115495f01993e4ca0671579
Jalberth Mosquera <jmosquera2305@gmail.com>
2026-09-26 11:47:17 +0200

    test: avoid nested shell expansion

--- tests/linux-smoke.sh
+++ tests/linux-smoke.sh
@@ -16,11 +16,11 @@ run_smoke() {
             export NVIM_APPNAME=mosquera-release-smoke
             ./install workstation
             command -v fish
-            test "\$HOME/.config/fish/config.fish" -ef /home/mosquera/environment/config/fish/config.fish
+            test /home/mosquera/.config/fish/config.fish -ef /home/mosquera/environment/config/fish/config.fish
             command -v tmux
-            test "\$HOME/.tmux.conf" -ef /home/mosquera/environment/config/tmux/tmux.conf
+            test /home/mosquera/.tmux.conf -ef /home/mosquera/environment/config/tmux/tmux.conf
             command -v nvim
-            test "\$HOME/.config/nvim" -ef /home/mosquera/environment/config/nvim
+            test /home/mosquera/.config/nvim -ef /home/mosquera/environment/config/nvim
             git status --porcelain > /tmp/mosquera-git-status
             test ! -s /tmp/mosquera-git-status
             ./doctor --profile workstation
